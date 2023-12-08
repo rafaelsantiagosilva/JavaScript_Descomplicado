36738b0dee600fbe5a40ab96d121c055d5443151
rafaelsantiagosilva <rafael.santiago.silva.1405@gmail.com>
2023-12-08 11:54:15 -0300

    Update calculadora.js

--- Aulas/04_JavaScript/15_JavaScriptExterno/calculadora.js
+++ Aulas/04_JavaScript/15_JavaScriptExterno/calculadora.js
@@ -33,7 +33,7 @@ function total() {
           res.value = parseFloat(v1.value) + parseFloat(res.value);
      } else if (op.value === "-") {
           res.value = parseFloat(v1.value) - parseFloat(res.value);
-     } else if (op.value === "*") {
+     } else if (op.value === "X") {
           res.value = parseFloat(v1.value) * parseFloat(res.value);
      } else if (op.value === "/") {
           if(res.value != 0){
@@ -49,5 +49,10 @@ function operacao(ope){
      v1.value = res.value;
      op.value = ope;
      res.value = "0";
+}
+
+function retornar(){
+     window.opener.valor.value = res.value;
+     window.close();
 }
 
