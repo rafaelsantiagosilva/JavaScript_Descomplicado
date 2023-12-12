235e7e600cb58b438f155daf8c5739de339d1c1d
rafaelsantiagosilva <rafael.santiago.silva.1405@gmail.com>
2023-12-12 18:08:13 -0300

    Concluindo EX04 e EX05 de jQuery

--- Vamos Praticar!/05_jQuery/04_05_HorarioAtual/script.js
+++ Vamos Praticar!/05_jQuery/04_05_HorarioAtual/script.js
@@ -12,7 +12,12 @@ $(document).ready(() => {
 				// Recebendo no HTML
 				$("#hour").html(dataHora.slice(0, 5));
 				// Convertendo a parte das horas em números
-				console.log(Number(dataHora.slice(0, 2)));
+				let hora_num = Number(dataHora.slice(0, 2));
+				if(hora_num >= 6 && hora_num < 18 && hora_num > 0){
+					$("#hour_image").css('background-image', "url(./images/morning.jpeg)");
+				} else{
+					$("#hour_image").css('background-image', "url(./images/night.jpeg)")
+				}
 			},
 		});
 	});
